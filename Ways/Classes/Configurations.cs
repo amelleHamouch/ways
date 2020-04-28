718c5449b8e144cfa561fcbf1c8f3fa23d435c23
amelle <amellehamouch@gmail.com>
2020-04-28 18:16:32 +0200

    connection Bdd

---
+++ Ways/Classes/Configurations.cs
@@ -0,0 +1,11 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace Ways.Classes
+{
+    class Configurations
+    {
+        public static MySqlConnection connection = new MySqlConnection("server = 127.0.0.1; database=ways;uid=root;pwd=root");
+
+    }
+} 
+
