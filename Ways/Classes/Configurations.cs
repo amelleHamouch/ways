395b1f7a70b6242279bec9710009d3a72c1b8666
amelle <amellehamouch@gmail.com>
2020-05-25 10:45:24 +0200

    CHG-adminViewStat

--- Ways/Classes/Configurations.cs
+++ Ways/Classes/Configurations.cs
@@ -6,7 +6,10 @@ namespace Ways.Classes
 {
     class Configurations
     {
-        public static MySqlConnection connection = new MySqlConnection("server =127.0.0.1;user=root;database=ways;port=3306;");
+        /// <summary>
+        /// Paramètre de connexion à la base de données
+        /// </summary>
+        public static MySqlConnection connection = new MySqlConnection("server=127.0.0.1;user=root;database=ways;port=3306;password=root;");
 
         public static implicit operator Configuration(Configurations v)
         {
--- Ways/Vues/AdminViewStats.xaml.cs
+++ Ways/Vues/AdminViewStats.xaml.cs
@@ -22,5 +22,7 @@ namespace Ways.Vues
         {
             InitializeComponent();
         }
+
+    
     }
 }
